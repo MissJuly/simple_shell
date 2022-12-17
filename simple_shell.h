5234f97c45381426d6d2d45d8a42e73f05824e8d
MissJuly <joygitau98@gmail.com>
2022-12-17 23:32:21 +0300

    Add embedded1

--- simple_shell.h
+++ simple_shell.h
@@ -173,14 +173,14 @@ int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
-/* toem_builtin.c */
-int _myexit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
+/* tour_embedded.c */
+int _exit(info_t *);
+int _cd(info_t *);
+int _help(info_t *);
 
-/* toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
+/* tour_embedded1.c */
+int _history(info_t *);
+int _alias(info_t *);
 
 /*toem_getline.c */
 ssize_t get_input(info_t *);
