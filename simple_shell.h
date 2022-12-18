5e7f664937392bf5701b37ccc98b8f5f7ebcd763
MissJuly <joygitau98@gmail.com>
2022-12-18 15:32:24 +0300

    Add more strings file

--- simple_shell.h
+++ simple_shell.h
@@ -131,13 +131,13 @@ int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
-/* toem_string.c */
+/* tour_strings.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
 
-/* toem_string1.c */
+/* tour_more_strings.c */
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
 void _puts(char *);
